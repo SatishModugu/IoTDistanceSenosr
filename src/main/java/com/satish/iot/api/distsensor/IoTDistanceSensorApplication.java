32c94a60883838415d171ea299a43074ee0e8ed0
smodugu <smodugu@IST242-NUC011.unomaha.edu>
2020-02-05 12:25:25 -0600

    UPdates

--- src/main/java/com/satish/iot/api/distsensor/IoTDistanceSensorApplication.java
+++ src/main/java/com/satish/iot/api/distsensor/IoTDistanceSensorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IoTDistanceSensorApplication {
 
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(IoTDistanceSensorApplication.class, args);
 	}
--- src/main/java/com/satish/iot/api/distsensor/data/SensorValue.java
+++ src/main/java/com/satish/iot/api/distsensor/data/SensorValue.java
@@ -12,34 +12,48 @@ public class SensorValue {
 	public static GpioPinDigitalOutput pin;
 	public static GpioPinDigitalOutput gpioTrigger;
 	public static GpioPinDigitalInput gpioEcho;
-
+    
+	//Takes triggerPin and echoPin as arguments.
 	public static String getDistance(Pin triggerPin, Pin echoPin) {
-
+		//getting the instance of GPIO Controller to acces GPIO pins of rasperyPI.
 		GpioController gpio = GpioFactory.getInstance();
 		if (gpioTrigger == null)
+			//Making the trigger Pin as output pin
 			gpioTrigger = gpio.provisionDigitalOutputPin(triggerPin);
 		if (gpioEcho == null)
-			gpioEcho = gpio.provisionDigitalInputPin(echoPin, PinPullResistance.PULL_DOWN); // Echo pin as																							// INPUT
+			//Making the echo Pin as input pin
+			gpioEcho = gpio.provisionDigitalInputPin(echoPin, PinPullResistance.PULL_DOWN); 
+		// Initializing start and end times
 		long startTime = 0;
 		long endTime = 0;
 		while (true) {
 			try {
+				//waiting for 2 seconds
 				Thread.sleep(2000);
-				gpioTrigger.high(); // Make trigger pin HIGH
-				Thread.sleep((long) 0.01);// Delay for 10 microseconds
-				gpioTrigger.low(); // Make trigger pin LOW
-				while (gpioEcho.isLow()) { // Wait until the ECHO pin gets HIGH
+				// Make trigger pin HIGH
+				gpioTrigger.high(); 
+				// Delay for 10 microseconds
+				Thread.sleep((long) 0.01);
+				// Make trigger pin LOW
+				gpioTrigger.low(); 
+				// Wait until the ECHO pin gets HIGH
+				while (gpioEcho.isLow()) { 
 				}
-				startTime = System.nanoTime(); // Store the current time to calculate ECHO pin HIGH time.
-				while (gpioEcho.isHigh()) { // Wait until the ECHO pin gets LOW
+				// Store the current time to calculate ECHO pin HIGH time.
+				startTime = System.nanoTime(); 
+				// Wait until the ECHO pin gets LOW
+				while (gpioEcho.isHigh()) { 
 				}
-				endTime = System.nanoTime(); // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
-				System.out.println("Distance :" + ((((endTime - startTime) / 1e3) / 2) / 29.1) + " cm"); //distance  in cm
+				 // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
+				endTime = System.nanoTime();
+				//distance  in cm
+				System.out.println("Distance :" + ((((endTime - startTime) / 1e3) / 2) / 29.1) + " cm"); 
 				Thread.sleep(1000);
 
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			//Returning distance.
 			return "Distance :" + ((((endTime - startTime) / 1e3) / 2) / 29.1) + " cm";
 		}
 
